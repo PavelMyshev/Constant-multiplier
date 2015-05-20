b4dd643a4b895b9b2c70f356ba1b2b952d623562
Pavel Myshev <fentles07@gmail.com>
2015-05-20 04:41:40 +0400

    second commit

--- js/main.js
+++ js/main.js
@@ -1,54 +1,113 @@
-var snap = Snap(1200,1200);
+var snap = Snap(3000,3000);
 
-var base = 4;
-var n = 5;
+function getDecimal(num) { // после запятой
+
+  var temp = ( (num - Math.floor(num)).toFixed(1) )[2];
+  return 1 * temp;
+}
+
+function getFloor(num) { // целая часть
+  return Math.floor(num);
+}  
+
+var base = 4;  // система исчисления
+var n = 5;     // множитель
+
+var tableB = [
+             
+             
+             [     0.0, 0.1, 0.2, 0.3, 1.0],
+             [     1.1, 1.2, 1.3, 2.0, 2.1],
+             [     2.2, 2.3, 3.0, 3.1, 3.2],
+             [     3.3, 4.0, 4.1, 4.2, 4.3],
+
+                            ];
 
   
-var space = 10;          // расстояние между линиями
-var middleSpace = 250;   // место для центральной части умножителя
-var xPoint = 100;        // начальная 'x' координата
-var yPoint = 100;		 // начальная 'y' координата
-var busLength = 1150;    // длина шин (вход, выход и т.д.)
-var lineAttr = {stroke: '#686868',strokeWidth: 2};
-var rectWidth = 3;
+var space = 10;                // расстояние между линиями
+var middleSpace = 250;         // место для центральной части умножителя
+var xPoint = 100;              // начальная 'x' координата
+var yPoint = 100;		           // начальная 'y' координата
+var busLength = 1150;          // длина шин (вход, выход и т.д.)
+var logicGateAttr = {fill:"rgb(255,255,255)", stroke: "#000", strokeWidth: 2};
+var rectWidth = 4;
 var amendment = rectWidth/2;
+
+// **********************
+// *** ОСНОВНЫЕ ЛИНИИ ***
+// **********************
+
 var inputLine = [];      // линии входа 
 var cLine = [];          // линии С
 var hLine = [];          // линии H
 var outputLine = [];     // линии выхода
-var upperOutLine = [];   // горизонтальные линии, идущие на выход
+
+// ************************
+// *** ПЕРЕМЕННЫЕ БЛОКА ***
+// ************************
+
+var upperOutLine = [];        // горизонтальные линии, идущие на выход
 for (var i = 0; i < n; i++){
        upperOutLine[i] = [];
        for (var j = 0; j < base; j++){
            upperOutLine[i][j] = 0;
    }}
-var upperHLine = [];     // горизонтальные линии, идущие на H
+
+var upperHLine = [];          // горизонтальные линии, идущие на H
 for (var i = 0; i < n; i++){
        upperHLine[i] = [];
        for (var j = 0; j < n; j++){
            upperHLine[i][j] = 0;
    }}
-var underRectLine = [];  // линия под прямоугольниками, соединяющая C и H входы
-var lowerInLine = [];    // горизонтальные линии, идущие на вход
+
+var underRectLine = [];       // линия под прямоугольниками, соединяющая C и H входы
+
+var lowerInLine = [];         // горизонтальные линии, идущие на вход
 for (var i = 0; i < n; i++){
        lowerInLine[i] = [];
        for (var j = 0; j < base; j++){
            lowerInLine[i][j] = 0;
    }}
 
-var inToOutLine = [];    // вертикальные линии, соединяющие вход и выход
-var cToHLine = [];       // вертикальные линии, соединяющие C и H
+var lowerInToOutLine = [];    // вертикальные линии, соединяющие вход и выход(часть под вентилем)
+for (var i = 0; i < n; i++){
+       lowerInToOutLine[i] = [];
+       for (var j = 0; j < base; j++){
+           lowerInToOutLine[i][j] = 0;
+   }}
+
+var upperInToOutLine = [];    // вертикальные линии, соединяющие вход и выход(часть над вентилем)
+for (var i = 0; i < n; i++){
+       upperInToOutLine[i] = [];
+       for (var j = 0; j < base; j++){
+           upperInToOutLine[i][j] = 0;
+   }}   
+
+var lowerCToHLine = [];       // вертикальные линии, соединяющие C и H(часть под вентилем)
+for (var i = 0; i < n; i++){
+       lowerCToHLine[i] = [];
+       for (var j = 0; j < base; j++){
+           lowerCToHLine[i][j] = 0;
+   }}
+
+var upperCToHLine = [];       // вертикальные линии, соединяющие C и H(часть над вентилем)
+for (var i = 0; i < n; i++){
+       upperCToHLine[i] = [];
+       for (var j = 0; j < base; j++){
+           upperCToHLine[i][j] = 0;
+   }}   
 
 
-var logicGate = [];
+var logicGate = [];     // логические вентили "AND"
    for (var i = 0; i < n; i++){
        logicGate[i] = [];
        for (var j = 0; j < base; j++){
            logicGate[i][j] = 0;
    }}
 
-var state = 0;
-var DiLine = {
+
+// линия, вместе с соединительными узлами
+var DiLine = {         
 	constructor: function(x1, y1, x2, y2, ln1, ln2, rn1, rn2){
 		this.line = snap.paper.line(x1, y1, x2, y2).attr({stroke: '#686868',strokeWidth: 2});
 		
@@ -61,6 +120,7 @@ var DiLine = {
 		return this;
 			 }
 	};
+  
 			
 
 // рисуем линии входа
@@ -100,7 +160,9 @@ for (var i = 0; i < base; i++){
     outputLine[i] = Object.create(DiLine).constructor(xPoint,yPoint, xPoint, busLength);
     xPoint += space;
 }
-while(state < n) {
+
+var block = 0;
+while(block < n) {
 // сделаем двойной пробел между линиями C и верхними горизонтальными линиями
 xPoint = parseFloat(cLine[n-1].line.attr("x1")) + space;
 yPoint += 2*space;
@@ -108,7 +170,7 @@ yPoint += 2*space;
 
 // верхние горизонтальные линии, идущие на выход
 for (var i = 0; i < base; i++){
-	upperOutLine[state][i] = Object.create(DiLine).constructor(xPoint, yPoint, outputLine[i].line.attr("x1"), yPoint, outputLine[i].line.attr("x1"), yPoint);
+	upperOutLine[block][i] = Object.create(DiLine).constructor(xPoint, yPoint, outputLine[i].line.attr("x1"), yPoint, outputLine[i].line.attr("x1"), yPoint);
 	yPoint += space;
 }
 
@@ -116,30 +178,64 @@ yPoint += space;
 
 // верхние горизонтальные линии, идущие на H
 for (var i = 0; i < n; i++){
-  	upperHLine[state][i] = Object.create(DiLine).constructor(xPoint,yPoint, hLine[i].line.attr("x1"), yPoint, hLine[i].line.attr("x1"), yPoint);
+  	upperHLine[block][i] = Object.create(DiLine).constructor(xPoint,yPoint, hLine[i].line.attr("x1"), yPoint, hLine[i].line.attr("x1"), yPoint);
   	yPoint += space;
   }
 
 xPoint += space;
 //создаем прямоугольники
  for (var i = 0; i < base; i++){
- 	logicGate[state][i] = snap.paper.rect(xPoint, yPoint, 45, 25).attr({fill:"rgb(255,255,255)", stroke: "#000", strokeWidth: 2});
- 	var t1 = snap.paper.text(xPoint+3, yPoint+15, '&'+state+','+i);
+ 	logicGate[block][i] = snap.paper.rect(xPoint, yPoint, 45, 25).attr(logicGateAttr);
+ 	var t1 = snap.paper.text(xPoint+3, yPoint+15, '&'+block+','+i);
  	xPoint += 45;
  }
 
-yPoint = parseFloat(logicGate[state][0].attr("y")) + 25 + space;
+yPoint = parseFloat(logicGate[block][0].attr("y")) + 25 + space;
 // нижняя CH линия
-underRectLine[state] = Object.create(DiLine).constructor(cLine[n-1-state].line.attr("x1"), yPoint, hLine[n-1-state].line.attr("x1"), yPoint,
-													 cLine[n-1-state].line.attr("x1"), yPoint, hLine[n-1-state].line.attr("x1"), yPoint);
+underRectLine[block] = Object.create(DiLine).constructor(cLine[n-1-block].line.attr("x1"), yPoint, hLine[n-1-block].line.attr("x1"), yPoint,
+													 cLine[n-1-block].line.attr("x1"), yPoint, hLine[n-1-block].line.attr("x1"), yPoint);
 yPoint += space;
 
  // нижние линии, идущие на вход
 for (var i = 0; i < base; i++){
-	lowerInLine[state][i] = Object.create(DiLine).constructor(inputLine[i].line.attr("x1"), yPoint, hLine[0].line.attr("x1") - space, yPoint,
+	lowerInLine[block][i] = Object.create(DiLine).constructor(inputLine[i].line.attr("x1"), yPoint, hLine[0].line.attr("x1") - space, yPoint,
 															  inputLine[i].line.attr("x1"), yPoint);
 	yPoint += space;
 }
 
-state++;
+//вертикальные линии, соединяющие вход и выход
+for (var i = 0; i < base; i++){
+  var q = getFloor(tableB[i][block]);
+  var x = getDecimal(tableB[i][block]);
+  var x1 = parseFloat(logicGate[block][i].attr("x"))+ space;
+  var y1 = lowerInLine[block][i].line.attr("y1");
+  var x2 = parseFloat(logicGate[block][i].attr("x"))+ space;
+  var y2 = parseFloat(logicGate[block][i].attr("y"))+25;
+   lowerInToOutLine[block][i] = Object.create(DiLine).constructor(x1,y1,x2,y2,x1,y1);
+   y2 = parseFloat(logicGate[block][i].attr("y"));
+   y1 = upperOutLine[block][Math.abs(base-1-x)].line.attr("y1");
+   
+   upperInToOutLine[block][i] = Object.create(DiLine).constructor(x1,y1,x2,y2,x1,y1);
+
+   // вертикальные линии, соединяющие C и H
+   x1 = parseFloat(logicGate[block][i].attr("x"))+ 3*space;
+   x2 = parseFloat(logicGate[block][i].attr("x"))+ 3*space;
+   y1 = underRectLine[block].line.attr("y1");
+   y2 = parseFloat(logicGate[block][i].attr("y"))+25;
+   lowerCToHLine[block][i] = Object.create(DiLine).constructor(x1,y1,x2,y2,x1,y1);
+   y2 = parseFloat(logicGate[block][i].attr("y"));
+   y1 = upperHLine[block][Math.abs(n-1-q)].line.attr("y1");
+   upperCToHLine[block][i] = Object.create(DiLine).constructor(x1,y1,x2,y2,x1,y1);
 }
+
+block++;
+}
+
+
+
+document.getElementById("myBtn").addEventListener("click", function() {
+
+  inputLine[0].line.attr({stroke: '#D00000'});  
+
+
+  }); 
